ed593bb8c32dbe059e65f9d4c77f56adaf38a622
LUISG47 <jl.galindez@live.com>
2025-02-12 22:12:23 -0600

    C01203
    C01203

---
+++ Postgree DB/schema_meteorite.sql
@@ -0,0 +1,13 @@
+CREATE TABLE meteorite_landings ( 
+name TEXT, 
+id INTEGER,
+nametype TEXT, 
+recclass TEXT, 
+mass REAL, 
+fall TEXT, 
+year INTEGER, 
+reclat REAL, 
+reclong REAL, 
+GeoLocation TEXT );
+
+SELECT * FROM meteorite_landings
