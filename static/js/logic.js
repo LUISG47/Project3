bd4e299a8b164fe01965bc89a9591bd59a91569f
LUISG47 <jl.galindez@live.com>
2025-02-13 20:45:17 -0600

    C13FEBFN02
    C13FEBFN02

--- static/js/logic.js
+++ static/js/logic.js
@@ -44,7 +44,6 @@ function createFeatures(meteoriteData) {
 var meteoriteIcon = new L.Icon({
     iconUrl: 'https://tinypic.host/images/2025/02/14/Met.png',
     iconSize: [40, 40], // size of the icon
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
